a073589f194c932fb2a0875d7e3fff4ec04716cf
TrachScor6 <liam.huynh@mail.utoronto.ca>
2025-05-03 20:19:23 -0400

    defence cards work
    using defence cards works! correct sprites and positions, just no collisions yet, but updates cards in hand properly.
    
    - implement selector buttons
    - implement collisions and damage

--- Jester-JameGam42/Assets/Scripts/GameManager.cs
+++ Jester-JameGam42/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Collections.Generic;
 using System;
 using System.Collections;
+using System.Linq;
 
 public class GameManager : MonoBehaviour {
 
@@ -45,6 +46,8 @@ public class GameManager : MonoBehaviour {
         StartCoroutine(LevelFlow());
     }
 
+
+    // Level Flow and timings
     private IEnumerator LevelFlow() {
         yield return new WaitForSeconds(5f);
         yield return StartCoroutine(RunLevel(1));
@@ -65,13 +68,15 @@ public class GameManager : MonoBehaviour {
         CancelInvoke(nameof(SpawnAttackCard));
     }
 
+
+    // Spawn attack cards
     private void SpawnAttackCard() {
         int random = UnityEngine.Random.Range(0, 13);
         bool goodSpotToSpawn = false;
         Vector2 direction = Vector2.zero;
         playerRigidBody = player.GetComponent<Rigidbody2D>();
         while (!goodSpotToSpawn) {
-            direction = pickDirection();
+            direction = pickDirectionHelper();
             Vector2 directionToPlayer = direction - new Vector2(playerRigidBody.position.x, playerRigidBody.position.y);
             float magDir = directionToPlayer.magnitude; 
             if (magDir > 2) {
@@ -81,47 +86,53 @@ public class GameManager : MonoBehaviour {
         GameObject summonedAttackCard = Instantiate(AttackCardPrefab, direction, Quaternion.identity);
         summonedAttackCard.GetComponent<AttackCard>().Initialize(random);
         Destroy(summonedAttackCard, 1f);
-
     }
 
-    private Vector2 pickDirection() {
+    private Vector2 pickDirectionHelper() {
         randomX = UnityEngine.Random.Range(-7.5f, 7.5f);
         randomY = UnityEngine.Random.Range(-2f, 2f);
         return new Vector2(randomX, randomY);
     }
 
+
+    // spawning defense cards per level into hand
     private void SpawnDefenseCards(int levelNum) {
         int cardPositionInHand = cardsInHand.Count;
         if (cardPositionInHand >= maxCardsInHand) {
             return;
         }
         int cardsToAdd = Mathf.Min(levelNum + 1, maxCardsInHand - cardPositionInHand);
-
         for (int i = 0; i < cardsToAdd; i++) {
-            AddCardToHand(i, cardPositionInHand);
+            AddCardToHand(i, player.cardsLeft);
             player.cardsLeft++;
-            cardPositionInHand++;
         }
     }
 
-    private void AddCardToHand(int counter, int cardPositionInHand) {
+    private void AddCardToHand(int spritePicker, int cardPositionInHand) {
         GameObject cardInHand = Instantiate(HandCardPrefab, new Vector2((-4f + (1.5f*cardPositionInHand)), -4f), Quaternion.identity);
         cardInHand.GetComponent<SpriteRenderer>().sortingOrder = 10;
-        cardInHand.GetComponent<DefenceCard>().Initialize(counter);
+        cardInHand.GetComponent<DefenceCard>().Initialize(spritePicker);
         cardsInHand.Add(cardInHand);
+        Debug.Log(string.Join(", ", cardsInHand.Select(card => card.name)));
     }
 
+
+    // use defence card from hand and spawn it in level
     public void UseDefenceCard(int cardInHandIndex, Vector2 lookDirection, Vector2 playerRigidBodyPosition) {
-        // summon as defence card
-        DefenceCard selectedCard = cardsInHand[cardInHandIndex].GetComponent<DefenceCard>();
-        cardsInHand.RemoveAt(cardInHandIndex);
         
+        Debug.Log("Want to use defence card");
+        cardInHandIndex = 0;
+        // get defence card from hand
+        DefenceCard selectedCard = cardsInHand[cardInHandIndex].GetComponent<DefenceCard>();
+        
+        // summon hand card as defence card
         float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
         GameObject summonedDefendCard = Instantiate(DefenceCardPrefab, playerRigidBodyPosition + lookDirection.normalized, Quaternion.Euler(0, 0, angle));
         summonedDefendCard.GetComponent<DefenceCard>().Initialize(selectedCard.GetHealth());
         Destroy(summonedDefendCard, 1.5f); // destroys object after 1.5 seconds
-        Destroy(selectedCard); 
-        player.cardsLeft--;
+
+        Debug.Log("Used defence card");
+
         UpdateCardsInHand(cardInHandIndex); // re-instantiate the hand? or instantiate from index
     }
 
@@ -131,22 +142,42 @@ public class GameManager : MonoBehaviour {
     }
 
     private void UpdateCardsInHand(int cardInHandIndex) {
-        // edge case at end of list
-        // if (cardInHandIndex == cardsInHand.Count) {
-        //     cardInHandIndex--;
-        //     return;
-        // // edge case no cards left
-        // } else if (cardsInHand.Count == 0) {
-        //     return;
-        // } else {
-        // // can destroy all and reform list    not this
-        // // or can destroy single and shift all right elements to the left by one (single already destroyed, list updated, so just destroy and instantiate starting at counter)
-        //     for (int counter = cardInHandIndex; counter < cardsInHand.Count; counter++) {
-        //         int tempCardHealth = cardsInHand[counter].GetComponent<DefenceCard>().GetHealth(); 
-        //         Destroy(cardsInHand[counter]);
-        //         AddCardToHand(tempCardHealth, counter);
-        //     }
-        // }
+        // destroy hand card, update cards left
+        GameObject tempToDestroy = cardsInHand[cardInHandIndex];
+        cardsInHand.RemoveAt(cardInHandIndex);
+        Destroy(tempToDestroy); 
+        player.cardsLeft--;
+
+        // edge case cards in hand = 0
+        if (cardsInHand.Count == 0) {
+            return;
+        // edge case destroyed end of list
+        } else if (cardInHandIndex == cardsInHand.Count) {
+            cardInHandIndex--;
+            return;
+        
+        } else {
+            Debug.Log("running the else");
+        // else, not end of list, still cards left, so shift rest of the cards over (list is already updated, just destroy old and instantiate new starting at index)
+        // can destroy all and reform list    not this
+        // or can destroy single and shift all right elements to the left by one (single already destroyed, list updated, so just destroy and instantiate starting at counter)
+            for (int counter = cardInHandIndex; counter < cardsInHand.Count; counter++) {
+                // destroy old game object
+                int tempCardHealth = cardsInHand[counter].GetComponent<DefenceCard>().GetHealth(); 
+                Destroy(cardsInHand[counter]);
+
+                // crete new game object at new position
+                GameObject cardInHand = Instantiate(HandCardPrefab, new Vector2((-4f + (1.5f*counter)), -4f), Quaternion.identity);
+                cardInHand.GetComponent<SpriteRenderer>().sortingOrder = 10;
+                cardInHand.GetComponent<DefenceCard>().Initialize(tempCardHealth);
+
+                // replace list object with new pointer
+                cardsInHand.RemoveAt(counter);
+                cardsInHand.Insert(counter, cardInHand);
+            }
+            Debug.Log("After update, cardInHandIndex is: " + cardInHandIndex + " and cards in hand looks like:");
+            Debug.Log(string.Join(", ", cardsInHand.Select(card => card.name)));
+        }
     }
 
 
